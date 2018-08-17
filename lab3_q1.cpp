2bd172106dd7ced7a10b1a920c28d7dcd2d797f3
anirudhbhat2k <42376037+anirudhbhat2k@users.noreply.github.com>
2018-08-17 22:01:46 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"Size of integer is "<<sizeof(a)<< "bytes"<<endl;
+char b;
+cout<<"Size of character is "<<sizeof(b)<<"bytes"<<endl;
+bool c;
+cout<<"Size of Boolean is "<<sizeof(c)<<"bytes"<<endl;
+float d;
+cout<<"size of float is "<<sizeof(d)<<"bytes"<<endl;
+double e;
+cout<<"Size of double is "<<sizeof(e)<<"bytes"<<endl;
+return 0;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int sum = 12+5;
+cout<<"The sum of 12 and 5 is " <<sum<<endl;
+int minus = 12-5;
+cout<<"The difference of 12 and 5 is " <<minus<<endl;
+int divide =12/6;
+cout<<"12 divided by 6 gives "<<divide<<endl;
+int modulus =12%5;
+cout<<"The modulus of 12 and 5 is "<<modulus<<endl;
+return 0;
+}
